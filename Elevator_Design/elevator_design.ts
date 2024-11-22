d75179dc3865897954e468f1ef68c7b5b47e3768
MSKU786 <manishsingh332000@gmail.com>
2024-11-22 11:16:11 +0530

    Added all the necessary classes

--- Elevator_Design/elevator_design.ts
+++ Elevator_Design/elevator_design.ts
@@ -62,4 +62,99 @@ class Elevator {
     else if (targetFloor < this.state.currentFloor) this.state.direction = Direction.DOWN;
     else this.state.direction = Direction.IDLE;
   }
+
+  // Get current elevator state
+  getState(): ElevatorState {
+    return this.state;
+  }
+
+  // Get elevator ID
+  getId(): number {
+    return this.id;
+  }
+}
+
+
+class SmartDispatcher {
+  private elevators: Elevator[];
+
+  constructor(elevators: Elevator[]) {
+    this.elevators = elevators;
+  }
+
+  assingElevator(floor:  number, direction: Direction) {
+    let bestElevator: Elevator|null = null;
+    let bestScore = Infinity;
+
+    for (let elevator of this.elevators) {
+      let state = elevator.getState();
+
+      let currentFloor = state.currentFloor;
+      let distance = Math.abs(currentFloor - floor);
+
+      //calcuation best elevator based on direction and distance 
+      const score =
+      state.isIdle ||
+      (state.direction === direction && ((direction === Direction.UP && state.currentFloor <= floor) || (direction === Direction.DOWN && state.currentFloor >= floor)))
+        ? distance
+        : Infinity;
+
+      if (score > bestScore) {
+        bestElevator = elevator;
+        bestScore = score
+      }   
+    }
+
+    if (bestElevator) {
+      bestElevator.scheduleStops(floor);
+    }
+
+    return bestElevator;
+  }
+}
+
+
+// Floor Class
+class Floor {
+  private floorNumber: number;
+
+  constructor(floorNumber: number) {
+    this.floorNumber = floorNumber;
+  }
+
+  // Call elevator
+  callElevator(dispatcher: SmartDispatcher, direction: Direction): void {
+    const elevator = dispatcher.assingElevator(this.floorNumber, direction);
+    console.log(`Elevator ${elevator!.getId()} assigned to Floor ${this.floorNumber}`);
+  }
+}
+
+class ElevatorSystem {
+  private elevators: Elevator[];
+  private floors: Floor[];
+  private dispatcher: SmartDispatcher;
+
+  constructor(numElevators: number, numFloors: number) {
+    let elevators: Elevator[] = []
+    for (let i=0; i<numElevators; i++) {
+      elevators.push(new Elevator(i+1));
+    }
+    this.elevators = elevators;
+    let floors : Floor[] = [];
+    for (let i=0; i<numFloors; i++)
+        floors.push(new Floor(i+1));
+    this.floors = floors;
+    this.dispatcher = new SmartDispatcher(this.elevators);
+  }
+
+    // Simulate the system
+    simulate(): void {
+      // Example: A user calls an elevator from the 5th floor to go up
+      this.floors[4].callElevator(this.dispatcher, Direction.Up);
+  
+      // Move elevators and handle their states
+      for (const elevator of this.elevators) {
+        elevator.move();
+      }
+    }
 }

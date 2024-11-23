bbcc177c2f794050056fcb7f4f7c1ca72a2c3d36
MSKU786 <manishsingh332000@gmail.com>
2024-11-23 11:35:25 +0530

    create chain of responsiblity desgin

--- Chain_Of_Responsblity/customer_support_system.ts
+++ Chain_Of_Responsblity/customer_support_system.ts
@@ -5,22 +5,89 @@
 */
 
 interface SupportHandler {
-  handleRequest(request: Request);
+  handleIssue(request: Issue);
   setNextHandler(nextHandler: SupportHandler);
 }
 
 class LevelOneHandler implements SupportHandler {
   private nextHandler: SupportHandler;
 
-  handleRequest(request: Request) {
-    console.log('Request handled by Level One');
-
-    if (this.nextHandler != null) {
-      this.nextHandler.handleRequest(request);
+  handleIssue(request: Issue) {
+    if (request.getPriority() == Priorty.Low) {
+      console.log('Handled by level one handler');
+    } else if (this.nextHandler != null) {
+      this.nextHandler.handleIssue(request);
     }
   }
 
   setNextHandler(nextHandler: SupportHandler) {
     this.nextHandler = nextHandler;
   }
+}
+
+class LevelTwoHandler implements SupportHandler {
+  private nextHandler: SupportHandler;
+
+  handleIssue(request: Issue) {
+    if (request.getPriority() == Priorty.Medium) {
+      console.log('Handled by level two handler');
+    } else if (this.nextHandler != null) {
+      this.nextHandler.handleIssue(request);
+    }
+  }
+
+  setNextHandler(nextHandler: SupportHandler) {
+    this.nextHandler = nextHandler;
+  }
+}
+
+class LevelThreeHandler implements SupportHandler {
+  private nextHandler: SupportHandler;
+
+  handleIssue(request: Issue) {
+    if (request.getPriority() == Priorty.High) {
+      console.log('Handled by level three handler');
+    } else if (this.nextHandler != null) {
+      this.nextHandler.handleIssue(request);
+    }
+  }
+
+  setNextHandler(nextHandler: SupportHandler) {
+    this.nextHandler = nextHandler;
+  }
+}
+
+enum Priorty {
+  Low = 'low',
+  Medium = 'Medium',
+  High = 'High',
+}
+
+class Issue {
+  private priority: Priorty;
+
+  constructor(priority: Priorty) {
+    this.priority = priority;
+  }
+
+  getPriority() {
+    return this.priority;
+  }
+}
+
+function main() {
+  const levelOneHadler = new LevelOneHandler();
+  const levelTwoHandler = new LevelTwoHandler();
+  const levelThreeHandler = new LevelThreeHandler();
+
+  levelOneHadler.setNextHandler(levelOneHadler);
+  levelTwoHandler.setNextHandler(levelThreeHandler);
+
+  const request1 = new Issue(Priorty.Low);
+  const request2 = new Issue(Priorty.Medium);
+  const request3 = new Issue(Priorty.High);
+
+  levelOneHadler.handleIssue(request1);
+  levelTwoHandler.handleIssue(request2);
+  levelThreeHandler.handleIssue(request3);
 }

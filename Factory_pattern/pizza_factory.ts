f0fda25862d3d9bd2b3cb2c76aa8b7a72d57eaea
MSKU786 <manishsingh332000@gmail.com>
2025-02-07 07:28:31 +0530

    Added another necessary classes NYPizzaStore ChicagoPizzaStore etc

--- Factory_pattern/pizza_factory.ts
+++ Factory_pattern/pizza_factory.ts
@@ -1,17 +1,44 @@
 abstract class PizzaStore {
-  public orderPizza(type: string): Pizza {
+  public orderPizza(type: string): Pizza | null {
     const pizza = this.createPizza(type);
+    return pizza;
   }
 
-  protected abstract createPizza(type: string): Pizza;
+  protected abstract createPizza(type: string): Pizza | null;
 }
 
+class NYPizzaStore extends PizzaStore {
+  protected createPizza(type: string): Pizza | null {
+    if (type === 'cheese') {
+      return new NYStyleCheesePizza();
+    } else if (type === 'veggie') {
+      return new NYStyleVeggiePizza();
+    } else {
+      return null;
+    }
+  }
+}
+
+class ChicagoPizzaStore extends PizzaStore {
+  protected createPizza(type: string): Pizza | null {
+    if (type === 'cheese') {
+      return new ChicagoStyleCheesePizza();
+    } else if (type === 'veggie') {
+      return new ChicagoStyleVeggiePizza();
+    } else {
+      return null;
+    }
+  }
+}
+
+// Start with an abstract Pizza class and all the concrete pizza classes will extend this class
 abstract class Pizza {
   name: string;
   dough: string;
   sauce: string;
   toppings: string[] = [];
 
+  // prepare follows a number of steps in a particular sequence(template method)
   prepare() {
     console.log('Preparing ' + name);
     console.log('Tossing dough...');
@@ -22,6 +49,7 @@ abstract class Pizza {
     }
   }
 
+  // the abstract class provide some basic defaults like bake , cut and box
   bake() {
     console.log('Bake for 25  mintues of 350');
   }
@@ -36,5 +64,52 @@ abstract class Pizza {
 
   getName(): string {
     return this.name;
+  }
+}
+
+class NYStyleCheesePizza extends Pizza {
+  public NYStyleChessePizza() {
+    this.name = 'NY Style Sauce and Cheese Pizza';
+    this.dough = 'Thin Crust Dough';
+    this.sauce = 'Marinara Sauce';
+
+    this.toppings.push('grated Reggiano Cheese');
+  }
+}
+class NYStyleVeggiePizza extends Pizza {
+  public NYStyleChessePizza() {
+    this.name = 'NY Style Sauce and Veggie Pizza';
+    this.dough = 'Thin Crust Dough';
+    this.sauce = 'Marinara Sauce';
+
+    this.toppings.push('grated Reggiano Veggie');
+  }
+}
+
+class ChicagoStyleCheesePizza extends Pizza {
+  public ChicagoStyleCheesePizza() {
+    this.name = 'Chicago Style Deep Dish Cheese Pizza';
+    this.dough = 'Extra Thick Crush Dough';
+    this.sauce = 'Plum Tomato Sauce';
+    this.toppings.push('Shredded Mozzarella Cheese');
+  }
+
+  // Overriding the default cut method
+  cut() {
+    console.log('Cutting the pizza into square slices');
+  }
+}
+
+class ChicagoStyleVeggiePizza extends Pizza {
+  public ChicagoStyleVeggiePizza() {
+    this.name = 'Chicago Style Deep Dish Veggie Pizza';
+    this.dough = 'Extra Thick Crush Dough';
+    this.sauce = 'Plum Tomato Sauce';
+    this.toppings.push('Shredded Mozzarella Veggie');
+  }
+
+  // Overriding the default cut method
+  cut() {
+    console.log('Cutting the pizza into square slices');
   }
 }

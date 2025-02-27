98bda1bc0f863ca3be44a674bba8fba14c23fd0c
MSKU786 <manishsingh332000@gmail.com>
2025-02-27 11:04:56 +0530

    Added different fly behaviour and quack Behaviour along with MallardDuck implemenation

--- Strategy_Pattern/mini_duck_simulator.ts
+++ Strategy_Pattern/mini_duck_simulator.ts
@@ -21,10 +21,47 @@ interface FlyBehaviour {
   fly(): void;
 }
 
+class FlyWithWings implements FlyBehaviour {
+  fly(): void {
+    console.log('I am flying');
+  }
+}
+
+class FlyNoWay implements FlyBehaviour {
+  fly(): void {
+    console.log("I can't fly");
+  }
+}
+
 interface QuackBehaviour {
   quack(): void;
 }
 
+class Quack implements QuackBehaviour {
+  quack(): void {
+    console.log('Quack Quack');
+  }
+}
+
+class MuteQuack implements QuackBehaviour {
+  quack(): void {
+    console.log('Silence');
+  }
+}
+
+class Squeak implements QuackBehaviour {
+  quack(): void {
+    console.log('Squeak');
+  }
+}
+
 class MallardDuck extends Duck {
-  display(): void {}
+  constructor() {
+    super();
+    this.quackBehavior = new Quack();
+    this.flyBehaviour = new FlyWithWings();
+  }
+  display(): void {
+    console.log('I am real Mailward Duck');
+  }
 }

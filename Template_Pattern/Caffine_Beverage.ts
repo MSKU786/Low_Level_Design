d932ab8145f1cbc9007be8f09a49a6200a844c5f
MSKU786 <manishsingh332000@gmail.com>
2025-03-16 12:47:07 +0530

    Added coffee or tea concrete classes

--- Template_Pattern/Caffine_Beverage.ts
+++ Template_Pattern/Caffine_Beverage.ts
@@ -18,3 +18,33 @@ abstract class CaffineBeverage {
     return true;
   }
 }
+
+class Coffee extends CaffineBeverage {
+  brew(): void {
+    console.log('Dripping coffee through filter');
+  }
+
+  addCondiments(): void {
+    console.log('Adding Sugar and Milk');
+  }
+}
+
+class Tea extends CaffineBeverage {
+  brew(): void {
+    console.log('Steep tea in boiling water');
+  }
+
+  addCondiments(): void {
+    console.log('Add lemons');
+  }
+}
+
+class BeverageTestDrive {
+  static main() {
+    let coffee: Coffee = new Coffee();
+    coffee.prepareRecipe();
+
+    let tea: Tea = new Tea();
+    tea.prepareRecipe();
+  }
+}

a7b0e3ca6ca9b0d35cc781d3ed37f1948199359e
MSKU786 <manishsingh332000@gmail.com>
2025-05-31 10:34:31 +0530

    Added Add function

--- LRU_Cache/index.ts
+++ LRU_Cache/index.ts
@@ -14,12 +14,14 @@ class LNode<K> {
 class LRUCache<K>{
   private size = 0;
   private currentSize = 0;
-  private head = null;
-  private tail = null;
+  private head :  LNode<K> | null;
+  private tail :  LNode<K> | null;
   private map = new Map<K, LNode<K>>()
 
   constructor(size: number) {
     this.size = size;
+    this.tail = null;
+    this.head = null;
   }
 
 
@@ -27,9 +29,54 @@ class LRUCache<K>{
     return this.size;
   }
 
-  add( node : LNode<K>) {
+  add( node : LNode<K>) : number {
+    let nodeData = node.value;
+
+    if (this.map.has(nodeData)) {
+        this.removeNodeFromList(nodeData);
+        this.addNodeOnList(nodeData);
+    } else {
+      this.currentSize++;
+      if (this.head == null) {
+        this.head = new LNode<K>(nodeData);
+        this.tail = this.head;
+      }
+
+      if (this.currentSize > this.size) {
+        this.map.delete(this.head.value);
+        this.head = this.head.next;
+      }
+    }
+
+    return 1;
+  }
+
+  removeNodeFromList(nodeData: K) {
+      let removeNode = this.map.get(nodeData);
+      let prevNode = removeNode?.prev;
+      let nextNode = removeNode?.next;
+
+      prevNode?.next = nextNode;
+      nextNode.prev = prevNode;
+  }
+
+  addNodeOnList(nodeData: K) {
+    let newNode = new LNode<K>(nodeData);
+
+    if (this.tail == null) {
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = this.tail.next;
+    }
+  }
+
+
+  remove(node: LNode<K>) {
 
   }
+
 
 
 }

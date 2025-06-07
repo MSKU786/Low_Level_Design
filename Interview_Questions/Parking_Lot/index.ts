f6f5f3ad26739eb77167308078184adaa3087f12
MSKU786 <manishsingh332000@gmail.com>
2025-06-07 15:28:13 +0530

    Added basic entities

--- Interview_Questions/Parking_Lot/index.ts
+++ Interview_Questions/Parking_Lot/index.ts
@@ -46,12 +46,43 @@ class ParkingSlot {
 class ParkingFloor {
   id: number;
   parkingSlots: ParkingSlot[] = [];
-
+  totalSlots: number;
   constructor(numberOfSlots: number) {
     for (let i = 0; i < numberOfSlots; i++) {
       this.parkingSlots.push(
         new ParkingSlot(`${this.id}+_+${i}`, VehicleType.TRUCK)
       );
     }
+
+    this.totalSlots = numberOfSlots;
+  }
+
+  isFull() {
+    let full = false;
+    for (let i = 0; i < this.totalSlots; i++)
+      if (this.parkingSlots[i].isFree()) return full;
+    return true;
   }
 }
+
+class ParkingLot {
+  static parkingLotInstance: ParkingLot | null = null;
+  parkingFloors: ParkingFloor[] = [];
+  totalFloors: number;
+  private constructor() {}
+
+  static getInstance(): ParkingLot {
+    if (this.parkingLotInstance != null) {
+      return this.parkingLotInstance;
+    }
+    return (this.parkingLotInstance = new ParkingLot());
+  }
+
+  addFloor(numberOfSlots) {
+    const floor = new ParkingFloor(numberOfSlots);
+    this.parkingFloors.push(floor);
+    this.totalFloors++;
+  }
+}
+
+class TicketService {}

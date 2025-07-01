6459b4631661bdf006cf53de3301056af4133dfc
MSKU786 <manishsingh332000@gmail.com>
2025-07-01 11:12:20 +0530

    Added parking spot manager classes

--- Interview_Questions/Parking_Lot/Design_Pattern_Based/ParkingSpotManger.ts
+++ Interview_Questions/Parking_Lot/Design_Pattern_Based/ParkingSpotManger.ts
@@ -1,9 +1,51 @@
 import { ParkingSpot } from './ParkingSpot';
+import { Vehicle } from './Vehicle';
 
-abstract class ParkingSpotManager {
+export abstract class ParkingSpotManager {
   parkingSpot: ParkingSpot[];
 
   constructor(parkingSpot: ParkingSpot[]) {
     this.parkingSpot = parkingSpot;
+  }
+
+  abstract findParkingSpot(): ParkingSpot | null;
+
+  parkVehicle(v: Vehicle): void {
+    //
+    let spot = this.findParkingSpot();
+    if (spot) {
+      spot.parkVehicle(v);
+    } else;
+    throw new Error('No parking spot available');
+  }
+
+  removeVehicle(v: Vehicle): void {
+    for (let spot of this.parkingSpot) {
+      if (spot.vehcile != null && spot.vehcile == v) {
+        spot.removeVehicle();
+      }
+    }
+  }
+}
+
+export class TwoWheelerParkingSpotManager extends ParkingSpotManager {
+  findParkingSpot(): ParkingSpot | null {
+    for (let spot of this.parkingSpot) {
+      if (spot.vehcile == null) {
+        return spot;
+      }
+    }
+    return null;
+  }
+}
+
+export class FourWheelerParkingSpotManager extends ParkingSpotManager {
+  findParkingSpot(): ParkingSpot | null {
+    for (let spot of this.parkingSpot) {
+      if (spot.vehcile == null) {
+        return spot;
+      }
+    }
+    return null;
   }
 }

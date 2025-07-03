79e95e68ee28fc90cdfd615859ebd790a026b31a
MSKU786 <manishsingh332000@gmail.com>
2025-07-03 08:03:31 +0530

    Started entrance Gate class

--- Interview_Questions/Parking_Lot/Design_Pattern_Based/EntranceGate.ts
+++ Interview_Questions/Parking_Lot/Design_Pattern_Based/EntranceGate.ts
@@ -1,1 +1,23 @@
-export class EntranceGate {}
+import { ParkingSpot } from './ParkingSpot';
+import { ParkingSpotManagerFactory } from './ParkingSpotManagerFactory';
+import { ParkingSpotManager } from './ParkingSpotManger';
+import { Ticket } from './Ticket';
+import { Vehicle } from './Vehicle';
+
+export class EntranceGate {
+  private factory: ParkingSpotManagerFactory;
+  private parkingSpotManager: ParkingSpotManager | null;
+  private ticket: Ticket | null;
+
+  constructor() {
+    this.factory = new ParkingSpotManagerFactory();
+  }
+
+  findParkingSpace(vehicle: Vehicle): ParkingSpot | null {
+    this.parkingSpotManager =
+      ParkingSpotManagerFactory.getParkingSpotManager(vehicle);
+    if (this.parkingSpotManager)
+      return this.parkingSpotManager.findParkingSpot();
+    return null;
+  }
+}

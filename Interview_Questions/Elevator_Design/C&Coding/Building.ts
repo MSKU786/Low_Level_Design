ef7f876f651a2044b0d351cb1b26a0f475c3b9c5
MSKU786 <manishsingh332000@gmail.com>
2025-07-28 10:27:59 +0530

    Added new classed initiating also implementation now

---
+++ Interview_Questions/Elevator_Design/C&Coding/Building.ts
@@ -0,0 +1,8 @@
+import { Floor } from './Floor';
+
+export class Building {
+  id: string;
+  name: string;
+  address: string;
+  floors: Floor[];
+}
---
+++ Interview_Questions/Elevator_Design/C&Coding/Floor.ts
@@ -0,0 +1,6 @@
+import { ExternalButton } from './External_Button';
+
+export class Floor {
+  id: number;
+  button: ExternalButton;
+}

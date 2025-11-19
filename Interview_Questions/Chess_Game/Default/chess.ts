8f0314da19b3af97e5a6dfbf8163e675e12793f3
MSKU786 <manishsingh332000@gmail.com>
2025-11-19 11:59:47 +0530

    Added differnt piece classes for chess lld

--- Interview_Questions/Chess_Game/Default/chess.ts
+++ Interview_Questions/Chess_Game/Default/chess.ts
@@ -1,37 +1,73 @@
-class Player {}
 
 abstract class Piece {
   isWhite: boolean;
   isKilled: boolean;
+  movementStrategy: MovementStrategy;
+  constructor(isWhite: boolean, movementStrategy) {
+    this.isWhite = isWhite;
+    this.isKilled = false;
+    this.movementStrategy: movementStrategy;
+  }
 
-  abstract isValidMove(): boolean;
+  setKilled() {
+    this.isKilled = true;
+  }
 }
 
 class King extends Piece {
+  constructor(isWhite: boolean, movemetnStrategy: MovementStrategy) {
+    super(isWhite, movemetnStrategy);
+  }
+
   isValidMove(): boolean {
     return false;
   }
 }
 
 class Queen extends Piece {
-  isValidMove(): boolean {
-    return false;
+
+  constructor(isWhite: boolean, movemetnStrategy: MovementStrategy) {
+    super(isWhite, movemetnStrategy);
   }
+
+
 }
 
 class Rook extends Piece {
-  isValidMove(): boolean {
-    return false;
+     constructor(isWhite: boolean, movemetnStrategy: MovementStrategy) {
+    super(isWhite, movemetnStrategy);
   }
+  }
+
 }
 
 class Pawn extends Piece {
-  isValidMove(): boolean {
-    return false;
+  constructor(isWhite: boolean, movemetnStrategy: MovementStrategy) {
+    super(isWhite, movemetnStrategy);
+  }
+
+
+
+}
+}
+
+class Bishop extends Piece {
+  constructor(isWhite: boolean, movemetnStrategy: MovementStrategy) {
+    super(isWhite, movemetnStrategy);
   }
 }
 
-class Board {}
+class King extends Piece {
+  constructor(isWhite: boolean, movemetnStrategy: MovementStrategy) {
+    super(isWhite, movemetnStrategy);
+  }
+}
+
+class Board {
+  cells: Cell[][];
+  player1: Player;
+  player2: Player;
+}
 
 class Cell {
   piece: Piece;
@@ -40,5 +76,46 @@ class Cell {
 
   getPiece(): Piece {
     return this.piece;
+  }
+}
+
+class Game {
+  board: Board;
+  p1: Player;
+  p2: Player;
+}
+
+class Player {
+  name: string;
+  email: string;
+  isWinner: boolean;
+}
+
+class MovementStrategy {
+  canMove(board, startCell: Cell, endCell: Cell) {}
+}
+
+class PieceFactory {
+  public static createPrice(
+    type: string,
+    isWhite: boolean,
+    movementStrategy: MovementStrategy
+  ) {
+    switch (type.toLowerCase()) {
+      case 'king':
+        return new King(isWhite, movementStrategy);
+
+      case 'queen':
+        return new Queen(isWhite, movementStrategy);
+
+      case 'rook':
+        return new Rook(isWhite, movementStrategy);
+
+      case 'bishop':
+        return new Bishop(isWhite, movementStrategy);
+
+      case 'knight':
+        return new Knight(isWhite, movementStrategy);
+    }
   }
 }
--- Patterns/Builder_Pattern/builder.ts
+++ Patterns/Builder_Pattern/builder.ts
@@ -29,3 +29,88 @@ Problem #2: Constructor Overloading and Huge Combinations ⚒️
 If a car has many optional attributes, you end up with multiple constructors, each for different combinations of parameters. This results in code duplication and leads to messy and unmanageable code.
 
 */
+
+class ICar {
+  private engine: string;
+  private wheels: number;
+  private seats: number;
+  private color: string;
+  private sunRoof: boolean;
+  private navigationSystem: boolean;
+
+  // Make constructor private so that only Builder can create a Car
+  protected constructor(builder: CarBuilder) {
+    this.engine = builder.engine;
+    this.wheels = builder.wheels;
+    this.seats = builder.seats;
+    this.color = builder.color;
+    this.sunRoof = builder.sunRoof;
+    this.navigationSystem = builder.navigationSystem;
+  }
+
+  // Expose a static method to start building
+  static builder() {
+    return new CarBuilder();
+  }
+
+  // Example method
+  displayInfo() {
+    console.log(
+      `Engine: ${this.engine}, Wheels: ${this.wheels}, Seats: ${this.seats}, Color: ${this.color}, Sunroof: ${this.sunRoof}, Navigation: ${this.navigationSystem}`
+    );
+  }
+}
+
+// Separate Builder class
+class CarBuilder {
+  engine!: string;
+  wheels: number = 4;
+  seats: number = 4;
+  color: string = 'black';
+  sunRoof: boolean = false;
+  navigationSystem: boolean = false;
+
+  setEngine(engine: string) {
+    this.engine = engine;
+    return this;
+  }
+
+  setWheels(wheels: number) {
+    this.wheels = wheels;
+    return this;
+  }
+
+  setSeats(seats: number) {
+    this.seats = seats;
+    return this;
+  }
+
+  setColor(color: string) {
+    this.color = color;
+    return this;
+  }
+
+  setSunRoof(hasSunRoof: boolean) {
+    this.sunRoof = hasSunRoof;
+    return this;
+  }
+
+  setNavigationSystem(hasNavigation: boolean) {
+    this.navigationSystem = hasNavigation;
+    return this;
+  }
+
+  build(): ICar {
+    return new ICar(this);
+  }
+}
+
+// ✅ Usage:
+const myCar = ICar.builder()
+  .setEngine('V8')
+  .setColor('red')
+  .setSunRoof(true)
+  .setNavigationSystem(true)
+  .build();
+
+myCar.displayInfo();

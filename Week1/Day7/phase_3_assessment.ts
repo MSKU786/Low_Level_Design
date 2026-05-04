10de0930dcb58869c6d13302366d99ae3a544a49
MSKU786 <manishsingh332000@gmail.com>
2026-05-04 10:33:03 +0530

    Added new phase 3 movie ticket booking system

---
+++ Week1/Day7/phase_3_assessment.ts
@@ -0,0 +1,70 @@
+enum SeatType {
+  REGULAR,
+  PRMEIMUM, 
+  VIP
+}
+
+class Seat {
+  type: SeatType
+  booked: boolean
+  number: string
+}
+
+class Ticket {
+  id: string
+  seats: Seat[]
+  movie: Movie
+}
+
+class Theatre {
+  id: string
+  seatMap: Seat[][]
+}
+
+class Movie {
+  name: string
+  genre: string[]
+  title: string
+  showTime: TimeRanges
+  duration: string
+
+}
+
+interface PaymentStrategy {
+  processPayment(amount: number)
+}
+
+class MovieFinder {
+  static search(keyword, date) {}
+}
+
+class CreditCardPayment implements PaymentStrategy {
+  constructor(private cardNumber) {}
+  processPayment(amount: number) {
+    return ;
+  }
+}
+
+class NetBanking implements PaymentStrategy {
+  constructor() {}
+  processPayment(amount: number) {
+    return;
+  }
+}
+
+
+interface CouponsStrategy {
+  applyCoupoun() 
+}
+
+
+class BookingSystem {
+
+  searchMovie(keyword, date) {
+    return MovieFinder.search(keyword, date)
+  }
+
+  bookTickets(movie) {
+    
+  }
+}

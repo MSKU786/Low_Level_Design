d043898dceafa63f71670265c8ed139d5e38773b
MSKU786 <manishsingh332000@gmail.com>
2026-05-09 16:19:50 +0530

    Added correct encausulated behaviour

---
+++ Week2/Day1/Encapsulation/Encapsulated.ts
@@ -0,0 +1,68 @@
+
+
+class BankAccount {
+  public readonly id: string;
+  public readonly ownerName: string;
+  public readonly createdAt: Date; 
+  private _balance: number;
+  private _transactions: Transaction[];
+  private _overdraftLimit: number;
+  private _frozen: boolean;
+
+  constructor(id: string, owner: string) {
+    this.id = id;
+    this.ownerName = owner;
+    this.createdAt = new Date();
+    this._balance = 0;
+    this._transactions = [];
+    this._overdraftLimit = 0;
+    this._frozen = false;
+  }
+
+
+  // Behaviours enforces rules not just set values
+
+  deposit(amount: number): void {
+    if (amount <= 0) {
+      throw new Error("Amount must be positive");
+    }
+
+    if (this._frozen) throw new Error("Account is frozen")
+
+    this._balance += amount;
+    this._transactions.push({
+      type: "deposit",
+      amount,
+      date: new Date()
+    })
+  }
+
+
+  withdraw(amount: number) : void {
+    if (amount <= 0) {
+      throw new Error("Amount must be positive");
+    }
+
+    if (this._frozen) throw new Error("Account is frozen")
+
+    if (amount > this._balance + this._overdraftLimit) {
+      throw new Error("Insufficient Balance")
+    }
+    this._balance -= amount;
+    this._transactions.push({
+      type: "withdrawl",
+      amount,
+      date: new Date()
+    })
+
+  }
+
+  freeze(): void {
+    this._frozen = true;
+  }
+
+  unfreeze(): void {
+    this._frozen = false;
+  }
+
+}

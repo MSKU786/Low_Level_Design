25a5347cbdc47d07a01b5edd66271a1e9ec1f4d7
MSKU786 <manishsingh332000@gmail.com>
2026-05-22 11:23:45 +0530

    Started with Week 2 Day 6 Challenge Library Management System

---
+++ Week2/Day6/Library_Management_System.ts
@@ -0,0 +1,6 @@
+/*
+
+Design and implement a library management system applying everything form Week 1 (SOLID) and Week 2 (OOP Pillars). Use the 
+5-step decomposition framework fro the hotel booking exercise. Start with the English sentence, then build from there
+
+*/

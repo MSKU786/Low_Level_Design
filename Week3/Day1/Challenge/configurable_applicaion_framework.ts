d3329e9a9e2bb159683508f2ec8309d98dfc06f1
MSKU786 <manishsingh332000@gmail.com>
2026-06-12 20:22:52 +0530

    Started with Plugin manager

--- Week3/Day1/Challenge/configurable_applicaion_framework.ts
+++ Week3/Day1/Challenge/configurable_applicaion_framework.ts
@@ -30,3 +30,50 @@ class ConfigMananger {
     ConfigMananger.instance = null;
   }
 }
+
+// Factory Method
+
+// Build a plugin system
+// 1. Plugins are created based on a type of string form a config file
+// 2. Pluygin types; "analytics', "logging", "caching", "auth"
+// 3. Each Plugin implemnts a Pluign interface with initalize() and execute()
+// 4. usE a registry factory -- adding a new plugin typeo requrieds zero edits
+// 5. the plugin manager loads plugin from config and intialized them
+
+class PluginFactory {
+  private pluggers = new Map<string, () => PluginStrategy>();
+
+  register(type: string, strategy: () => PluginStrategy): void {
+    this.pluggers.set(type, strategy);
+  }
+
+  create(method: string): PluginStrategy {
+    const creator = this.pluggers.get(method);
+    if (!creator) throw new Error(`Unknow ${method}`);
+    return creator;
+  }
+}
+
+interface Plugin {
+  intialize(): void;
+  execute(): void;
+}
+
+class AnalyticsPlugin implements Plugin {
+  intialize(): void {}
+
+  execute(): void {}
+}
+
+class AuthPlugin implements Plugin {
+  intialize(): void {}
+
+  execute(): void {}
+}
+
+class PluginManager {
+  constructor(private configManger: IConfig) {}
+}
+
+const config = ConfigMananger.getInstance();
+config.lo;

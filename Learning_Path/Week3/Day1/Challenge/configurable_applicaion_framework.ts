140aad43d4c0197ba6b40fdbdd7478967c81de50
MSKU786 <manishsingh332000@gmail.com>
2026-06-14 15:44:17 +0530

    Configurable application framework project ocmpleted

--- Learning_Path/Week3/Day1/Challenge/configurable_applicaion_framework.ts
+++ Learning_Path/Week3/Day1/Challenge/configurable_applicaion_framework.ts
@@ -2,6 +2,12 @@ interface IConfig {
   get<T>(key: string): T;
 }
 
+interface PluginConfig {
+  type: string;
+  enalbed: boolean;
+  setting: Record<string, unknown>;
+}
+
 class ConfigMananger {
   private config: Record<string, any> = {};
   private static instance: ConfigMananger | null = null;
@@ -40,40 +46,102 @@ class ConfigMananger {
 // 4. usE a registry factory -- adding a new plugin typeo requrieds zero edits
 // 5. the plugin manager loads plugin from config and intialized them
 
-class PluginFactory {
-  private pluggers = new Map<string, () => PluginStrategy>();
-
-  register(type: string, strategy: () => PluginStrategy): void {
-    this.pluggers.set(type, strategy);
-  }
-
-  create(method: string): PluginStrategy {
-    const creator = this.pluggers.get(method);
-    if (!creator) throw new Error(`Unknow ${method}`);
-    return creator;
-  }
-}
-
 interface Plugin {
-  intialize(): void;
-  execute(): void;
+  readonly name: string;
+  readonly versoin: string;
+  initalize(settings: Record<string, unknown>): Promise<void>;
+  execute(context: Record<string, unknown>): Promise<void>;
+  destroy(): Promise<void>;
 }
 
 class AnalyticsPlugin implements Plugin {
-  intialize(): void {}
+  readonly name: string = 'analytics';
+  readonly versoin: string = '1.0.0';
+  private endpoint: string = '';
 
-  execute(): void {}
+  async initalize(settings: Record<string, unknown>): Promise<void> {
+    this.endpoint =
+      (settings.endpoint as string) ?? 'https://analytics.exmaple.com';
+    console.log(` [Analytics] Initalized -> ${this.endpoint}`);
+  }
+
+  async execute(context: Record<string, unknown>): Promise<void> {
+    console.log('Analtyics tracking event');
+  }
+
+  async destroy(): Promise<void> {
+    console.log(`[Analytics] shutting down`);
+  }
 }
 
-class AuthPlugin implements Plugin {
-  intialize(): void {}
+class LoggingPlugin implements Plugin {
+  readonly name: string = 'logging';
+  readonly versoin: string = '1.0.0';
+  private endpoint: string = '';
 
-  execute(): void {}
+  async initalize(settings: Record<string, unknown>): Promise<void> {
+    this.endpoint =
+      (settings.endpoint as string) ?? 'https://logging.exmaple.com';
+    console.log(` [Logging] Initalized -> ${this.endpoint}`);
+  }
+
+  async execute(context: Record<string, unknown>): Promise<void> {
+    console.log(`[Analtyics] tracking event -> ${this.name}`);
+  }
+
+  async destroy(): Promise<void> {
+    console.log(`[Logging] shutting down`);
+  }
+}
+
+
+class PluginFactory {
+  private creators = new Map<string, () => Plugin>();
+
+  register(type: string, strategy: () => Plugin): void {
+    this.creators.set(type, strategy);
+  }
+
+  create(method: string): Plugin {
+    const creator = this.creators.get(method);
+    if (!creator) throw new Error(`Unknow ${method}`);
+    return creator();
+  }
+
+  getRegisteedTypes(): string[] {
+    return [...this.creators.keys()]
+  }
 }
 
 class PluginManager {
-  constructor(private configManger: IConfig) {}
+  private activePlugin: Plugin[] = [];
+
+  constructor(
+    private config: IConfig;
+    private factory: PluginFactory;
+  ) {}
+
+
+  // Read config -> create plugin -> initalize all
+  async loadPlugins(): Promise<void> {
+    const PluginConfigs = this.config.get<PluginConfig[]>("plugins");
+
+    console.log(`Loading ${PluginConfigs.length} plugin(s)....`)
+
+    for (const plugConfig of PluginConfigs) {
+      if (!plugConfig.enalbed) {
+        console.log( ` Skipping disbled plugin: ${plugConfig.type}`)
+        continue;
+      }
+
+      const plugin = this.factory.create(plugConfig.type);
+      await plugin.initalize(plugConfig.setting);
+      this.activePlugin.push(plugin); 
+    }
+
+    console.log(`${this.activePlugin.length} pluing(s) loaded and intialzed\n`)
+  }
+
+  
 }
 
-const config = ConfigMananger.getInstance();
-config.lo;

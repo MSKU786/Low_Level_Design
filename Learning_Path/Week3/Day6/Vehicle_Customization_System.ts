5280ff524aacb7f9a37677b6ab8ddfb7ba695ca7
MSKU786 <manishsingh332000@gmail.com>
2026-07-04 20:40:33 +0530

    Added vehicle speic and type registry

--- Learning_Path/Week3/Day6/Vehicle_Customization_System.ts
+++ Learning_Path/Week3/Day6/Vehicle_Customization_System.ts
@@ -17,24 +17,117 @@ class SportsEngine implements Engine {}
 
 class SportsSuspension implements Suspension {}
 
-class SportBrakes implements Brake {}
+class SportsBrakes implements Brake {}
 
-class SportTire implements Tire {}
+class SportsTire implements Tire {}
 
-class SportCarFactory implements VehicleFactory {
+class SportsCarFactory implements VehicleFactory {
   createEngine(): SportsEngine {
     return new SportsEngine();
   }
 
-  createBrake(): SportBrakes {
-    return new SportBrakes();
+  createBrake(): SportsBrakes {
+    return new SportsBrakes();
   }
 
-  createTire(): SportTire {
-    return new SportTire();
+  createTire(): SportsTire {
+    return new SportsTire();
   }
 
   createSuspension(): SportsSuspension {
     return new SportsSuspension();
+  }
+}
+
+class ComfortEngine implements Engine {}
+
+class ComfortSuspension implements Suspension {}
+
+class ComfortBrakes implements Brake {}
+
+class ComfortTire implements Tire {}
+
+class ComfortCarFactory implements VehicleFactory {
+  createEngine(): ComfortEngine {
+    return new ComfortEngine();
+  }
+
+  createBrake(): ComfortBrakes {
+    return new ComfortBrakes();
+  }
+
+  createTire(): ComfortTire {
+    return new ComfortTire();
+  }
+
+  createSuspension(): ComfortSuspension {
+    return new ComfortSuspension();
+  }
+}
+
+class OffroadEngine implements Engine {}
+
+class OffroadSuspension implements Suspension {}
+
+class OffroadBrakes implements Brake {}
+
+class OffroadTire implements Tire {}
+
+class OffroadCarFactory implements VehicleFactory {
+  createEngine(): OffroadEngine {
+    return new OffroadEngine();
+  }
+
+  createBrake(): OffroadBrakes {
+    return new OffroadBrakes();
+  }
+
+  createTire(): OffroadTire {
+    return new OffroadTire();
+  }
+
+  createSuspension(): OffroadSuspension {
+    return new OffroadSuspension();
+  }
+}
+
+
+interface Clonable<<T> {
+  clone(): T; 
+}
+
+
+class VehiclePrototype implements Clonable<VehiclePrototype> {
+  constructor(
+    public type: string,
+    public factory: VehicleFactory
+  ) {}
+
+  clone(): VehiclePrototype {
+    return new VehiclePrototype(this.type, this.factory)
+  }
+}
+
+
+interface VehicleSpec{
+  type: string;
+  maxPassenger: number;
+  cargoCapacity: number;
+  basePrice: number;
+}
+
+
+class VehicleTypeRegistry {
+  private specs = new Map<string, VehicleSpec>();
+
+  register(type: string, spec: VehicleSpec): void {
+    this.specs.get(type, spec);
+  }
+
+  get(type: string): VehicleSpec {
+    const spec = this.specs.get(type);
+    if (!spec) 
+      throw new Error(`Unkonw Vehichle type: ${type}`)
+    return spec;
   }
 }

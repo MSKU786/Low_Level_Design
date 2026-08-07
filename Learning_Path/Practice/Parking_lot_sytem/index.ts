0803e00ddf048e29b55949b150012c676bf0b053
MSKU786 <manishsingh332000@gmail.com>
2026-08-07 07:07:28 +0530

    Added class for Parking lot pricing strategy and pricing registry

--- Learning_Path/Practice/Parking_lot_sytem/index.ts
+++ Learning_Path/Practice/Parking_lot_sytem/index.ts
@@ -2,6 +2,10 @@ class Spot {
   type: SpotType;
   private isReserved = false;
 
+  constructor(private type: SpotType) {
+    this.type = type;
+  }
+
   isBooked() {
     return this.isReserved;
   }
@@ -30,11 +34,51 @@ enum VehicleType {
 class Vehicle {
   type: VehicleType;
   drivingLicense: string;
-  number_plate: string;
+  vechile_number: string;
 
   constructor(
     private type: VehicleType,
     private drivingLicense: string,
-    private number_plate: string,
+    private vechile_number: string,
   ) {}
 }
+
+interface PricingStrategy {
+  calculateFee(time: number): number;
+}
+
+class TwoWheelerPricing implements PricingStrategy {
+  calculateFee(time: number): number {
+    return time * 1;
+  }
+}
+
+class FourWheelerPricing implements PricingStrategy {
+  calculateFee(time: number): number {
+    return time * 2;
+  }
+}
+
+class EightWheelPricing implements PricingStrategy {
+  calculateFee(time: number): number {
+    return time * 4;
+  }
+}
+
+class PricingRegistry {
+  map = new Map<string, PricingStrategy>();
+
+  get(type: string) {
+    let strategy = this.map.get(type);
+    if (!strategy) {
+      throw new Error("type doesn't exist");
+    }
+    return strategy;
+  }
+
+  create(type: string, strategy: PricingStrategy) {
+    this.map.set(type, strategy);
+  }
+}
+
+class ParkingSystem {}

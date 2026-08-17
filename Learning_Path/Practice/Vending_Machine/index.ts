038e512495dff90a81e9d6b533d278357f26127e
MSKU786 <manishsingh332000@gmail.com>
2026-08-17 07:54:21 +0530

    Added correct approach to handle the scenaios

--- Learning_Path/Practice/Vending_Machine/index.ts
+++ Learning_Path/Practice/Vending_Machine/index.ts
@@ -1,11 +1,3 @@
-class VendingMachineContext {
-  state: State;
-
-  setState(state: State) {}
-
-  request(): void;
-}
-
 class Order {
   product: Product;
   quantity: number;
@@ -17,29 +9,106 @@ class Order {
   }
 }
 
-enum MACHINE_STATES {
-  IDLE,
-  PICK_PRODUCT,
-  ACCEPT_MONEY,
-  CANCEL_ORDER,
-}
-
 interface State {
-  handleRequest(): void;
+  setProduct(position: number, quantity: number): Order;
+  moneyInserted(amount: number): void;
+  cancelOrder(): void;
+  dispenseAndRefund(): void;
 }
 
-class ReadyState implements State {
-  handleRequest(): void {}
+class ReadyStatee implements State {
+  setProduct(position: number, quantity: number): Order {
+    return new Order();
+  }
+
+  moneyInserted(amount: number): void {
+    throw new Error('Wrong Action');
+  }
+
+  cancelOrder(): void {
+    throw new Error('Wrong Action');
+  }
+
+  dispenseAndRefund(): void {
+    throw new Error('Wrong Action');
+  }
 }
 
 class ProductSelectedState implements State {
-  handleRequest(): void {}
+  setProduct(position: number, quantity: number): Order {
+    return new Order();
+  }
+
+  moneyInserted(amount: number): void {
+    throw new Error('Wrong Action');
+  }
+
+  cancelOrder(): void {
+    throw new Error('Wrong Action');
+  }
+
+  dispenseAndRefund(): void {
+    throw new Error('Wrong Action');
+  }
 }
 
 class PaymentPendingState implements State {
-  handleRequest(): void {}
+  setProduct(position: number, quantity: number): Order {
+    return new Order();
+  }
+
+  moneyInserted(amount: number): void {
+    throw new Error('Wrong Action');
+  }
+
+  cancelOrder(): void {
+    throw new Error('Wrong Action');
+  }
+
+  dispenseAndRefund(): void {
+    throw new Error('Wrong Action');
+  }
 }
 
 class OutofStockState implements State {
-  handleRequest(): void {}
+  setProduct(position: number, quantity: number): Order {
+    return new Order();
+  }
+
+  moneyInserted(amount: number): void {
+    throw new Error('Wrong Action');
+  }
+
+  cancelOrder(): void {
+    throw new Error('Wrong Action');
+  }
+
+  dispenseAndRefund(): void {
+    throw new Error('Wrong Action');
+  }
 }
+
+class VendingMachine {
+  state: State;
+
+  constructor() {
+    this.state = new ReadyStatee();
+  }
+
+  setProduct() {}
+
+  insertMoney() {}
+
+  setState() {}
+
+  cancelOrder() {}
+
+  dispenseAndRefund() {}
+}
+
+const machine = new VendingMachine();
+
+machine.setProduct();
+machine.insertMoney();
+machine.insertMoney();
+machine.dispenseAndRefund();

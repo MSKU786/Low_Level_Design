7023cd9611bba2418d12a1c5979193b3298df797
MSKU786 <manishsingh332000@gmail.com>
2026-08-19 10:36:28 +0530

    Started with Different entiteis book , bookcopy and BookRecord

--- Learning_Path/Practice/Library_Management_System/index.ts
+++ Learning_Path/Practice/Library_Management_System/index.ts
@@ -18,5 +18,66 @@ class BookCOpy {
     this.isAvailble = true;
   }
 
+  checkAvailablity(): boolean {
+    return this.isAvailble;
+  }
+
+  private setAvailablity(isAvailble: boolean) : void {
+    this.isAvailble = isAvailble;
+  }
+
+  borrow(): void {
+    this.setAvailablity(false);
+  }
+
+  return(): void {
+    this.setAvailablity(true)
+  }
+}
+
+
+class User {
+  private name: string;
+  private email: string;
+  private idCard: string;
+  private borrowedBookCopies: BookCOpy[] = [];
+
+  constructor(name: string, emeail: string, idCard: string) {
+    this.name = name;
+    this.email = emeail;
+    this.idCard = idCard
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  borrowBook(bookCOpy: BookCOpy) : void {
+    if (this.borrowedBookCopies.length >=3 ) {
+      throw new Error("You have reached the maximum number of books you can borrow")
+    }
+
+    bookCOpy.borrow();
+    this.borrowedBookCopies.push(bookCOpy);
   
+  }
+
+
+  returnBook(bookCopyId: string) {
+    this.borrowedBookCopies = this.borrowedBookCopies.filter(b => b.id != bookCopyId) ;
+  }
+
+  getBorrowBooks(): BookCOpy[] {
+    return this.borrowedBookCopies;
+  }
+}
+
+
+class BookRecord {
+  id: string;
+  issueDate: string;
+  constructor(private userId: string, private bookCopyId: string) {
+    this.issueDate = Date.now();
+
+  }
 }

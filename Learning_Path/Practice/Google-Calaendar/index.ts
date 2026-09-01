92cdf989a71ba674c528d03806f33ccaabf2b5aa
MSKU786 <manishsingh332000@gmail.com>
2026-09-01 17:50:03 +0530

    Started with new practice question Google calendar

---
+++ Learning_Path/Practice/Google-Calaendar/index.ts
@@ -0,0 +1,9 @@
+/*
+
+Design a google calendar like system where users can:
+Create view and manage events
+any number of people can be added in an event
+share evetns wit view edit permissions 
+respond to evetn propse changes to evetns 
+find time based on the availablity of users
+*
